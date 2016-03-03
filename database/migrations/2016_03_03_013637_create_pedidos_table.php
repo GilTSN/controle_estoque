6b4aa1ec948ce75bf885fa92bdd45d48ea81351c
Gil <giltsn@giltsn.com.br>
2016-03-02 22:47:33 -0300

    Setting up database

---
+++ database/migrations/2016_03_03_013637_create_pedidos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('produto_id')->unsigned();
+            $table->integer('cliente_id')->unsigned();
+            $table->foreign('produto_id')->references('id')->on('produtos');
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pedidos');
+    }
+}
