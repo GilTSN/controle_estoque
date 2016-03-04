cc4079f5a0c106093ccbff8ef0d6996200384c9c
Gil <giltsn@gmail.com>
2016-03-04 19:14:53 -0300

    Developing the initial page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,21 @@
 @extends('templates.app')
 
 @section('content')
-    
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <a href="#" class="btn-block"><i class="glyphicon glyphicon-edit"></i> Pedidos</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <a href="#" class="btn-block"><i class="glyphicon glyphicon-inbox"></i> Estoque</a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
